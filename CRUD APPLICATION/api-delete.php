06aa054bc1e1026a775be4621153e920a7f23a0b
MuneebHassanSiddiqui <mmuneebhassan@gmail.com>
2024-07-18 03:34:35 +0500

    Working..

---
+++ CRUD APPLICATION/api-delete.php
@@ -0,0 +1,25 @@
+<?php
+include "connection/config.php";
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+if (!isset($data['Stu_id'])) {
+    echo json_encode(array("message" => "Student ID required", "status" => false));
+    exit();
+}
+
+$student_id = $data['Stu_id'];
+
+$mySql = "DELETE FROM student_information WHERE id = $student_id";
+
+if (mysqli_query($conn, $mySql)) {
+    echo json_encode(array("message" => "Record deleted", "status" => true));
+} else {
+    echo json_encode(array("message" => "Record not deleted", "status" => false));
+}
+
---
+++ CRUD APPLICATION/api-search.php
@@ -0,0 +1,24 @@
+<?php
+
+header('Content-Type: application/json');
+
+header('Access-Control-Allow-Origin: *');
+
+include "connection/config.php";
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+// $searchvalue = $data['search'];
+$searchvalue = isset($_GET['search']) ? $_GET['search'] : die();
+
+
+$sql = "SELECT * FROM student_information WHERE Full_Name LIKE '%$searchvalue%' ";
+
+$res = mysqli_query($conn, $sql) or die('Query Unsuccessfull!');
+
+if (mysqli_num_rows($res) > 0) {
+    $Fetch_data = mysqli_fetch_all($res, MYSQLI_ASSOC);
+    echo json_encode($Fetch_data);
+} else {
+    echo json_encode(array('message' => 'no Record Found', 'status' => false));
+}
